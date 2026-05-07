6db3e8d8234131c6c938b398b042ff0078d4549b
https://github.com/sinchana0913-prog/myprofile <sinchana0913@gmail.com>
2026-05-07 16:23:09 +0530

    about me added

--- js/data/about.js
+++ js/data/about.js
@@ -1,4 +1,4 @@
 document.getElementById("about").innerHTML = `
   <h2 class="text-3xl mb-4">About Me</h2>
   <p>I am a MERN stack developer passionate about building scalable applications.</p>
-`; 
+`;  
